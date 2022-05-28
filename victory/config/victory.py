6538b5937f343171af77a868fa6674b5eef16fdc
Atulsah <atul.sah7@gmail.com>
2022-05-28 12:31:52 +0530

    GoogleSheet Stock Entry - production - update 12

--- victory/config/victory.py
+++ victory/config/victory.py
@@ -4,6 +4,19 @@ from frappe import _
 
 def get_data():
 	return[
+		{
+			"label": _("Google Data"),
+			"icon": "icon-cog",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Production Entry",
+					"onboard": 1,
+					"dependencies": [],
+					"description": _("Production Entry"),
+				},	
+			]
+		},
         {	"label": _("Key Report"),
 			"icon": "icon-cog",
 			"items": [
@@ -13,6 +26,12 @@ def get_data():
 					"name": "Sales Order Status Report",
 					"doctype": "Sales Order",
 				},	
+				{
+					"type": "report",
+					"is_query_report": True,
+					"name": "Set Unset Report",
+					"doctype": "Sales Order",
+				},
 
 			]
 		}
