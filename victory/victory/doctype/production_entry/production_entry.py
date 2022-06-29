585daeeebf4a161ec1b97dc317af957fcc3fb5aa
ATUL SAH <Atulsah@users.noreply.github.com>
2022-06-29 16:47:43 +0530

    Update production_entry.py

--- victory/victory/doctype/production_entry/production_entry.py
+++ victory/victory/doctype/production_entry/production_entry.py
@@ -26,15 +26,7 @@ class ProductionEntry(Document):
 
         credentials = {
             "type": "service_account",
-            "project_id": "amplified-vine-352304",
-            "private_key_id": "bf45918c601308bc5fe59f7823a15a15cdd1ef60",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQC0Wf1ZhzR2/IkM\ngaaLmtjN5mPDO/aUZjmI+yzBghotQXonLYzzjLm0EvG5lQ+OXifI8KU5bEcpuANy\nz6bWp+MgMRmTizZdq/umJZi7UJplg92FkHOm0KKAvHL8Lz7esdzu2Et6bU8G2e/G\nmkIiaXKXOTHDSwrM6rRMtenSSTC07/QK0FjWA9LHFmXbUZjmPAs487oHM6De1+C8\nOgaR/eVGv9STYDnnIkKEyIBsz+E4n5p1Zua+iGK3SldmHiB2TqXFIsnvaJ1+TWlN\nsygJr+wIb9vbxQjxBEMDjPLaWqu/iOdFTkgvKzsF4Rva25rPH6jphXWVeDq+tAxa\nJYLORDD5AgMBAAECggEAECrGeydzgKyV8QLwzfAzOe7ql8Jo8efhhbnlatHZvJ+T\nXo525ZDCh2XQ2TbnanbeiGTpHMcL8I2UMo8yiKjLY5P/M9KdjKNZh0z0vmrk8k9y\nNNJMicJHZPSvtpNaJo6pbFhDmN2SYV35QHOM1OagvPVltHjmRpUOFVfT1nPU6801\ngtAOD7WUBl8NCgrVa/4Cd3gz3RgIPrK5AkIB6s1WR0Z9PIawft2ZI3uee17j2IoA\nR5X9/IGFckFQPLEQlRKX+dzHX78ZbneMAexPEAsywuq9xqklSrNdZqEhQdmBrHjE\nLgQ/FaBwQQ16vBFaiWPDHrtTqQSNTxokezKoomn8kQKBgQDywId2MD3inn+TAuEh\nMZaIT4I1IsPIXFB+we4U/BPiGzSX3Q4vVe6uZ8iC0VH/AvwqI8xONM0+i1I7rypn\nvNvHGI4H0M7yfPtPizx7yAHTWVqnLleKCH7x3OntGoWqaJccMH6vuDuCUQFEqvCy\nm9fZ5fpLiT06J00wt90KUd2yiQKBgQC+MarMnpwfbd2DIvVzS7hPfQaizKNsm27/\nrCeElw1lxVetEhfuuyOCjIJSHIYosiENfzL7Nu6W7o3/P7HfZwjWfqnOSAqoHCXS\n3R+VJtTqDDsGuHdzB6B0YF4O9gMTBXNClHZ6vwqzKtsAiGCT7QCNMuxbwfrKZhDP\nKtvObgau8QKBgBjbnEN/BydIobm1JsaWZFrLAYNdbvz6bwe75hxh/8s8i/MtIMG6\nI/naCM9Ujff2H0pVZhtwDsBj1eTkuAvta/gETK8CM97i630mvefTAFTbJoxOE4q/\n+ffcblvoRl2/3hF16dMmo3lhwCZ+9qzjhoP6p9nKUOm+kon1mf3viis5AoGAaJIk\nR4a5bipYzV4uN+sc9k9Wk7IR9IftWpOL0sG+cwMytoypLfAkkQWyLXVTahKuS55x\nKGBGhCO7XOjnYl5jJJPVxv4jJdT8EtjTIY2NKPX6ijJoMo2x8ep+LJmkjRPFEIlS\nLyDUwDfC26Mf/pUjZX7nJ3ga7Kd7fHwjCHfEIvECgYByihIqoLrYpYrHTSFwVTx0\ntTOtJgbvPLxvmmt3ollJJchjYuZxZQov78wI6ATE3svoQbzIP0SB6rTH6S1Vqnm1\nujplNKkxCPwBkM8pXIMbi7Ddv6lFz6sfP6QkINmkGFtjcRWJEigwNRPA9N6p4yoR\nlhFVTH7muAwHPvDXjQ2axQ==\n-----END PRIVATE KEY-----\n",
-            "client_email": "erp-viwl@amplified-vine-352304.iam.gserviceaccount.com",
-            "client_id": "100832027379896973953",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/erp-viwl%40amplified-vine-352304.iam.gserviceaccount.com"
+            
             }
 
         gc = gspread.service_account_from_dict(credentials)
